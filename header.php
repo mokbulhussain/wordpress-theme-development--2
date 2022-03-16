18fffa683e7959b3d445fa655e5681ae722a2104
mokbulhussain <mokbulhussain2@gmail.com>
2022-03-16 10:09:02 +0600

    first commit

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<!--
+	Launcher by freehtml5.co
+	Twitter: http://twitter.com/fh5co
+	URL: http://freehtml5.co
+-->
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Launcher &mdash; Free Website Template, Free HTML5 Template by FreeHTML5.co</title>
+
+<?php wp_head();?>
+
+	</head>
